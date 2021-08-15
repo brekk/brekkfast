394bea86a8872a13eb6ed44796eef5ed53dad0dc
brekk <brekk@brekkbockrath.com>
2021-08-14 19:11:35 -1000

    add eject.reprint

--- brekkfast.js
+++ brekkfast.js
@@ -1,9 +1,9 @@
-'use strict'
+'use strict';
 
-const raw = { yes: true }
-const other = { ...raw, cool: 'very' }
+const raw = { yes: true };
+const other = { ...raw, cool: 'very' };
 
 const BREKKFAST = () =>
-  `I am brekkfast, hear me roar ${JSON.stringify(other)}`
+  `I am brekkfast, hear me roar ${JSON.stringify(other)}`;
 
-module.exports = BREKKFAST
+module.exports = BREKKFAST;
--- brekkfast.umd.js
+++ brekkfast.umd.js
@@ -1,21 +1,15 @@
-;(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined'
-    ? (module.exports = factory())
-    : typeof define === 'function' && define.amd
-    ? define(factory)
-    : ((global =
-        typeof globalThis !== 'undefined'
-          ? globalThis
-          : global || self),
-      (global.brekkfast = factory()))
-})(this, function () {
-  'use strict'
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.brekkfast = factory());
+}(this, (function () { 'use strict';
 
-  const raw = { yes: true }
-  const other = { ...raw, cool: 'very' }
+  const raw = { yes: true };
+  const other = { ...raw, cool: 'very' };
 
   const BREKKFAST = () =>
-    `I am brekkfast, hear me roar ${JSON.stringify(other)}`
+    `I am brekkfast, hear me roar ${JSON.stringify(other)}`;
 
-  return BREKKFAST
-})
+  return BREKKFAST;
+
+})));
